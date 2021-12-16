4359dd1d3959566731719f8dd20f4f7a22de64f1
Jakob Lingel <jakob.lingel@zeiss.com>
2021-12-16 12:29:24 +0100

    cleanup

--- alternative/battleship/src/Server.java
+++ alternative/battleship/src/Server.java
@@ -67,28 +67,6 @@ public class Server {
 				connection.setMessage(Connection.getIn().readLine());
 				connection.setTurn(true);
 				if (connection.getMessage() == null) break;
-//				if (connection.getMessage().contains("ship")) {
-//					String msg = connection.getMessage();
-//					String[] temp = msg.split(" ");
-//					int shotx = Integer.valueOf(temp[1]);
-//					int shoty = Integer.valueOf(temp[2]);
-//					// check own ship array
-//					if (ships[shotx][shoty] == false) {
-//						((JButton)e.getSource()).setBackground(new Color(0,0,255));
-//						Connection.sendMessage("answer 0");
-//					} else {
-//						((JButton)e.getSource()).setBackground(new Color(255,0,0));
-//						// what does 1 2 etc mean?
-//						Connection.sendMessage("answer 1");
-//						hitCounter--;
-//						if(hitCounter==0) {
-//							frame.dispose();
-//							new Spielgui(8);
-//							return;
-//						}
-//					}
-//						Connection.sendMessage("answer 0");
-//				}
 				System.out.println("<<< " + connection.getMessage());
 			}
 
